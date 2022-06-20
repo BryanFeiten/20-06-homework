f5658c69e03a6b9cb59482876e592da8de57515b
Bryan <bryan.feiten@growdev.com.br>
2022-06-20 20:09:54 -0300

    initial configure for use mui

---
+++ src/config/styles/Globals.tsx
@@ -0,0 +1,15 @@
+import GlobalStyles from '@mui/material/GlobalStyles';
+
+const NewGlobalStyles = () => {
+	return (
+		<GlobalStyles styles={{
+			a: {
+				textDecoration: 'none',
+				margin: 0,
+				padding: 0,
+			}
+		}} />
+	);
+}
+
+export default NewGlobalStyles;
