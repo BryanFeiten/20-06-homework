daa3f2ca893413b59d1ad3314e50fc65d9907f86
Bryan <bryan.feiten@growdev.com.br>
2022-06-20 23:09:17 -0300

    altera modo de export, e também importação

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import Routers from './Routesrs';
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles'; 
 
-import ThemeDefault from './config/styles/themes/Default';
+import createTheme from './config/styles/themes/Default';
 import NewGlobalStyles from './config/styles/Globals';
 
-function App() {
+export default function App() {
   return (
-    <ThemeProvider theme={ThemeDefault}>
+    <ThemeProvider theme={createTheme}>
       <NewGlobalStyles />
       <Routers />
     </ThemeProvider>
   );
 }
-
-export default App;
